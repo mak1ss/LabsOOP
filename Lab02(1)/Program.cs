0167107e79d7122a55af9a2f14ac509fc6139b84
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-09-06 20:26:30 +0300

    Started lab2

---
+++ Lab02(1)/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+    public static void main(string[] args)
+    {
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        string[] shorter;
+        string[] longer;
+        if (input1.Length < input2.Length)
+        {
+            shorter = input1.Split(" ");
+            longer = input2.Split(" ");
+
+        }
+        else
+        {
+            shorter = input2.Split(" ");
+            longer = input1.Split(" ");
+        }
+
+
+        int count = 0;
+
+        for (int i = 0; i < shorter.Length; i++)
+        {
+            if (shorter[i].Equals(longer[i]))
+            {
+                count++;
+            }
+        }
+
+        int countReverse = 0;
+        for (int i = longer.Length - 1, j = shorter.Length - 1; j >= 0; i--, j--)
+        {
+            if (longer[i].Equals(shorter[j]))
+            {
+                countReverse++;
+            }
+        }
+
+        Console.WriteLine(count > countReverse ? count : countReverse);
+    }
+}
