ad6a153320bf97516cdcfe555dfc68386e5cd3ac
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-09-06 22:01:00 +0300

    Solved the second problem

---
+++ Lab02(2)/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arrayInput = Console.ReadLine().Split(" ");
+
+        int r = int.Parse(Console.ReadLine());
+
+        int[] array = new int[arrayInput.Length];
+
+        int[] sum = new int[arrayInput.Length];
+
+        for (int i = 0; i < arrayInput.Length; i++)
+        {
+            array[i] = int.Parse(arrayInput[i]);
+        }
+        int temp;
+        for (int i = 0; i < r; i++)
+        {
+            temp = array[array.Length - 1];
+            for(int j = array.Length - 1; j > 0; j--)
+            {
+                array[j] = array[j - 1];
+            }
+            array[0] = temp;
+
+            if (i >= 1)
+            {
+                for(int j = 0; j < array.Length; j++)
+                {
+                    sum[j] += array[j];
+                }
+            } else
+            {
+                for(int j = 0; j < array.Length; j++)
+                {
+                    sum[j] = array[j];
+                }
+            }
+            
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(sum[i] + " ");
+        }
+    }
+}
