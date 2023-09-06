9f04a5b2ef6bfa6d167a7839bb3b1ff4c45b32b6
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-09-06 23:25:38 +0300

    Solved 3d problem (Lab 2)

---
+++ Lab02(3)/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int k = input.Length / 4;
+        int[] upperArray = new int[input.Length / 2];
+        for (int i = 0, j = k - 1, g = upperArray.Length - 1, f = input.Length - k; i < k; i++, j--, g--, f++)
+        {
+            upperArray[i] = int.Parse(input[j]);
+            upperArray[g] = int.Parse(input[f]);
+        }
+
+        int[] lowerArray = new int[upperArray.Length];   
+        for(int i = k, j = 0; i < input.Length - k; i++, j++)
+        {
+            lowerArray[j] = int.Parse(input[i]);
+        }
+
+        for(int i = 0; i < upperArray.Length; i++)
+        {
+            Console.Write(upperArray[i] + lowerArray[i] + " ");
+        }
+    }
+}
