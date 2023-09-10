a441aaf0b2ea63bbd50e4b4cd87f31d8cc98d4aa
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-09-10 19:16:28 +0300

    Solved 10th problem

---
+++ Lab02(10)/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Trim().Split(" ");
+        int diff = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[input.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(input[i]);
+        }
+
+        int pairs = 0;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for(int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] - arr[j] == diff || arr[j] - arr[i] == diff)
+                {
+                    pairs++;
+                }
+            }
+        }
+        Console.WriteLine(pairs);
+    }
+}
