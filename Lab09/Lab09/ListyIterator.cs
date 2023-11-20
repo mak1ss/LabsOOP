0fa8014134e245cf8f34ce048e2f848cdcaef256
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-11-20 16:13:19 +0200

    Solve 2 problems

---
+++ Lab09/Lab09/ListyIterator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    internal class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+
+        private int position = 0;
+
+        public bool HasNext
+        {
+            get
+            {
+                return position < collection.Count - 1;
+            }
+        }
+
+        public ListyIterator(List<T> collection)
+        {
+            this.collection = collection;
+        }
+
+        public bool MoveNext()
+        {
+            if (HasNext)
+            {
+                position++;
+                return true;
+            } 
+            return false;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 1)
+            {
+                throw new ArgumentException("Invalid operation!");
+            }
+            Console.WriteLine(collection[position]);
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in collection)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach(T item in collection)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ Lab09/Lab09/Program.cs
@@ -0,0 +1,50 @@
+﻿using Lab09;
+using System.Text.RegularExpressions;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Match match = Regex.Match(input, "Create\\s{\\s*([^}]+)\\s*}");
+        if (!match.Success)
+        {
+            Console.Error.WriteLine("Invalid creation input");
+            Environment.Exit(0);
+        }
+        string elements = match.Groups[1].Value.Trim();
+        ListyIterator<string> iterator;
+        if (string.IsNullOrEmpty(elements))
+        {
+            iterator = new ListyIterator<string>(new List<string>());
+        }
+        else
+        {
+            iterator = new ListyIterator<string>(elements.Split(" ").ToList());
+        }
+
+        while (!(input = Console.ReadLine()).Equals("END"))
+        {
+            switch (input)
+            {
+                case "Move":
+                    Console.WriteLine(iterator.MoveNext());
+                    break;
+                case "Print":
+                    iterator.Print();
+                    break;
+                case "HasNext":
+                    Console.WriteLine(iterator.HasNext);
+                    break;
+                case "PrintAll":
+                    foreach(string item in iterator)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Unknown operation");
+                    break;
+            }
+        }
+    }
+}
