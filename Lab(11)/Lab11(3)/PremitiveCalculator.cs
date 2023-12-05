c1f5817337444f3fa049af699df919fc5f8bfd3c
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-12-05 10:36:48 +0200

    Solve 3d problem lab 11th

---
+++ Lab(11)/Lab11(3)/PremitiveCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_3_
+{
+
+    internal class PremitiveCalculator
+    {
+        private Dictionary<char, PerformCalculation> strategies;
+
+        private delegate int PerformCalculation(int firstOperand, int secondOperand);
+
+        private PerformCalculation currentStrategy;
+
+        public PremitiveCalculator()
+        {
+            strategies = new Dictionary<char, PerformCalculation>
+            {
+                { '+', Add },
+                { '-', Subtract },
+                { '*', Multiply},
+                { '/', Divide}
+            };
+            currentStrategy = Add;
+        }
+        public void ChangeStrategy(char @operator)
+        {
+            if (!strategies.ContainsKey(@operator))
+            {
+                throw new InvalidOperationException($"Unknown strategy: \"{@operator}\"");
+            }
+            currentStrategy = strategies[@operator];
+        }
+
+        public int? Calculate(int firstOperand, int secondOperand)
+        {
+            return currentStrategy?.Invoke(firstOperand, secondOperand);
+        }
+
+        private int Add(int firstOperand, int secondOperand)
+        {
+            return firstOperand + secondOperand;
+        }
+
+        private int Subtract(int firstOperand, int secondOperand)
+        {
+            return firstOperand - secondOperand;
+        }
+
+        private int Multiply(int firstOperand, int secondOperand)
+        {
+            return firstOperand * secondOperand;
+        }
+
+        private int Divide(int firstOperand, int secondOperand)
+        {
+            if (secondOperand == 0)
+            {
+                throw new DivideByZeroException("Divide by 0. Second operand should't be zero");
+            }
+            return firstOperand / secondOperand;
+        }
+    }
+}
---
+++ Lab(11)/Lab11(3)/Program.cs
@@ -0,0 +1,49 @@
+﻿using Lab11_3_;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PremitiveCalculator calculator = new PremitiveCalculator();
+
+        string input;
+
+        while (!(input = Console.ReadLine()).ToLower().Equals("end"))
+        {
+            string[] @params = input.Split(' ');
+            if (@params[0].Equals("mode"))
+            {
+                if (@params[1].Length > 1)
+                {
+                    Console.WriteLine($"Operation name is too long: {@params[1]}");
+                    continue;
+                }
+                calculator.ChangeStrategy(@params[1][0]);
+            }
+            else
+            {
+                int firstOperand, secondOperand = 0;
+
+                if (int.TryParse(@params[0], out firstOperand) && int.TryParse(@params[1], out secondOperand))
+                {
+                    try
+                    {
+                        Console.WriteLine(calculator.Calculate(firstOperand, secondOperand));
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operands passed");
+                    continue;
+                }
+            }
+
+        }
+    }
+}
